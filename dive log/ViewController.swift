c121d42333f253443ad2c6e2b7c0d96cfdff9492
max <jung33@gmail.com>
2018-12-22 19:37:40 +0900

    Initial Commit

---
+++ dive log/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dive log
+//
+//  Created by max on 22/12/2018.
+//  Copyright © 2018 shc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
